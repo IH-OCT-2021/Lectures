b9494db5f6122d95470852317d5296283caf30c5
Mike <troianello@protonmail.com>
2021-10-22 09:45:43 -0700

    Filter and Sort included

--- 10-21/script.js
+++ 10-21/script.js
@@ -33,7 +33,7 @@
 // console.log("This is New Arr", newArr)
 // console.log("This is the second arr", secondArr)
 
-//Reduce 
+//Reduce
 
 // let numbers = ['f','o','o','b','a','r']
 
@@ -74,7 +74,6 @@
 
 // let neighbors = [user1,user2,user3, user4];
 
-
 // function checkForCars(neighbor){
 //   if(!neighbor.ownCar){
 //     return `${neighbor.name} does not own a car`
@@ -83,7 +82,156 @@
 //   }
 // }
 
-
 // let arrayOfPeopleWhoDontOwnCars = neighbors.map(checkForCars)
 
 // console.log("Car ownership", arrayOfPeopleWhoDontOwnCars)
+
+// let arr = [1,2,3,4,5,6,7,8];
+
+// //numbers greater than 4
+
+// let arrOfNumbersGreaterThanFour = arr.filter((num)=>{
+//   return num > 4
+// })
+
+// console.log(arrOfNumbersGreaterThanFour)
+
+// let colors = ['red', 'yellow', 'magenta', 'periwinkle', 'aubergine'];
+
+// let coolColors = colors.filter(function(color){
+//   console.log("This is the color", color)
+//   return color.length >=7
+// })
+
+// console.log(coolColors)
+
+// let car1 = {
+//   color: "red",
+//   maker: "Honda",
+//   year: 2014,
+// }
+// let car2 = {
+//   color: "blue",
+//   maker: "Ford",
+//   year: 1999,
+// }
+// let car3 = {
+//   color: "black",
+//   maker: "Tesla",
+//   year: 2019,
+// }
+// let car4 = {
+//   color: "red",
+//   maker: "Ferrari",
+//   year: 1981,
+// }
+// let car5 = {
+//   color: "silver",
+//   maker: "Tesla",
+//   year: 2021,
+// }
+
+// let carArr = [car1, car2, car3, car4, car5];
+
+// let isCarTesla = carArr.filter((car)=>{
+//   return car.maker === 'Tesla'
+// })
+
+// let oldCars = carArr.filter((car)=>{
+//   return car.year<2000
+// })
+
+// console.log("These cars are Teslas:", isCarTesla)
+
+// console.log("These are from the 20th century:", oldCars)
+
+//SORT
+
+//Sorts elements of an array
+
+// let arr = ['a','v','e','see','se','o']
+
+// let sorted = arr.sort()
+
+// let arr = [1,23,4,100,5,8,6,3,321]
+
+// let sorted = arr.sort()
+
+// console.log(sorted)
+
+//Sort usually needs a callback function
+
+// function compare(a, b) {
+//   if (a < b) {
+//     return -1;
+//   }
+//   if (a > b) {
+//     return 1;
+//   }
+//   // a must be equal to b
+//   return 0;
+// }
+
+// let arr = [1,23,4,100,5,8,6,3,321]
+
+// let sorted = arr.sort(compare)
+
+// console.log(sorted)
+
+//Sorting with Objects based on criteria
+
+// let car1 = {
+//   color: "red",
+//   maker: "Honda",
+//   year: 2014,
+// }
+// let car2 = {
+//   color: "blue",
+//   maker: "Ford",
+//   year: 1999,
+// }
+// let car3 = {
+//   color: "black",
+//   maker: "Tesla",
+//   year: 2019,
+// }
+// let car4 = {
+//   color: "red",
+//   maker: "Ferrari",
+//   year: 1981,
+// }
+// let car5 = {
+//   color: "silver",
+//   maker: "Tesla",
+//   year: 2021,
+// }
+
+// let carArr = [car1, car2, car3, car4, car5];
+
+// function compare(a, b) {
+//   if (a.year < b.year) {
+//     return -1;
+//   }
+//   if (a.year > b.year) {
+//     return 1;
+//   }
+//   // a must be equal to b
+//   return 0;
+// }
+
+// //These two options are functionally identical
+
+// let carsOrderedByYear = carArr.sort(compare)
+
+// let carsOrderedByYear = carArr.sort((a,b)=>{
+//    if (a.year < b.year) {
+//     return -1;
+//   }
+//   if (a.year > b.year) {
+//     return 1;
+//   }
+//   // a must be equal to b
+//   return 0;
+// })
+
+// console.log("This is ordered by year:", carsOrderedByYear)
